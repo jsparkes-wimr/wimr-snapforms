c346034a1746273703dcb8f69e92741a9f1e1de9
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-09-03 14:14:45 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -2,7 +2,11 @@
     //-------------------------------------------------------------------------------------
     var p=""; //put a unique prefix to avoid conflict with others 
     var H=$vm.hosting_path+"/modules";
+    var E="https://vmiis.github.io/wimr-email-templates"
+    var E="http://localhost:8000/vmiis/wimr-email-templates"
     var m=$vm.module_list;
-    var api="wapp";
+    var api="wimr";
     //-------------------------------------------------------------------------------------
+    m[p+"preview-email"]={url:H+"/business/preview-email/form.html",prefix:p};
+    m[p+"email-appointment-confirmation"]={url:E+"/appointment-confirmation.html"};
 })();
