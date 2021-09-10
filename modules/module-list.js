ec73eaaf566191118e6422193b79b92b17672a8c
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-09-10 12:31:31 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -3,13 +3,22 @@
     var p=""; //put a unique prefix to avoid conflict with others 
     var H=$vm.hosting_path+"/modules";
     var E="https://vmiis.github.io/wimr-email-templates"
-    var E="http://localhost:8000/vmiis/wimr-email-templates"
+    //var E="http://localhost:8000/vmiis/wimr-email-templates"
     var m=$vm.module_list;
     var api="wimr";
     //-------------------------------------------------------------------------------------
-    m[p+"preview-email"]={url:H+"/business/preview-email/form.html",prefix:p};
-    m[p+"email-appointment-confirmation"]={url:E+"/appointment-confirmation.html"};
-    m[p+"about_the_woolcock_pdf"]={url:E+"/about_the_woolcock.pdf"};
-    m[p+"our_billing_explained_pdf"]={url:E+"/our_billing_explained.pdf"};
+    m[p+"send_email"]={url:H+"/business/send-email/form.html",prefix:p,router:1};
+    m[p+"preview-appointment-confirmation"]={url:H+"/business/preview-email/appointment-confirmation.html",prefix:p};
+    m[p+"preview-consult-registration"]={url:H+"/business/preview-email/consult-registration.html",prefix:p};
+    m[p+"preview-sleep-registration"]={url:H+"/business/preview-email/sleep-registration.html",prefix:p};
+
+    
+    m[p+"template-appointment-confirmation"]={url:E+"/appointment-confirmation.html"};
+    m[p+"template-consult-registration"]={url:E+"/consult-registration.html"};
+    m[p+"template-sleep-registration"]={url:E+"/sleep-registration.html"};
+    
+    //m[p+"about_the_woolcock_pdf"]={url:E+"/about_the_woolcock.pdf"};
+    //m[p+"our_billing_explained_pdf"]={url:E+"/our_billing_explained.pdf"};
+    //m[p+"history"]={url:H+"/business/history/data.html",api:"wimr",Table:'email-log',router:1};
     
 })();
